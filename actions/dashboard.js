518270b4012f3fee743ab19843463a267c6a2cdd
Aditya Srivastava <adityasrivastav9721057380@gmail.com>
2025-04-28 15:34:40 +0530

    create new account in dashboard feature completed

--- actions/dashboard.js
+++ actions/dashboard.js
@@ -2,6 +2,15 @@
 
 import { db } from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
+
+const serializeTransaction = (obj) => {
+    const serialized = {...obj};
+
+    if(obj.balance) {
+        serialized.balance = obj.balance.toNumber();
+    }
+}
 
 export async function createAccount(data) {
     try {
@@ -29,8 +38,29 @@ export async function createAccount(data) {
 
         const shouldBeDefault = existingAccounts.length === 0 ? true : data.isDefault;
 
+        // if this account should be default, unset other default accounts
+        if (shouldBeDefault) {
+            await db.account.updateMany({
+                where: { userId: user.id, isDefault: true },
+                data: { isDefault: false},
+            })
+        }
+
+        const account = await db.account.create({
+            data: {
+                ...data,
+                balance: balanceFloat,
+                userId: user.id,
+                isDefault: shouldBeDefault,
+            }
+        })
+
+        const serializedAccount = serializeTransaction(account);
+
+        revalidatePath("/dashboard")
+        return { success: true, data: serializedAccount };
         
     } catch(error) {
-
+      throw new Error(error.message);
     }
 }
--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import Header from "@/components/header";
 import "./globals.css";
 import { Inter } from 'next/font/google'
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from 'sonner'
 
 const inter = Inter({subsets: ["latin"]})
 
@@ -20,6 +21,7 @@ export default function RootLayout({ children }) {
         {/* header */}
         <Header/>
         <main className="min-h-screen">{children}</main>
+        <Toaster richColors />
         
         {/* footer */}
         <footer className="bg-blue-50 py-12">
