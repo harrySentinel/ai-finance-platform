1ad5d62e345375560343ae0d5dab8a2a4e79e90a
Aditya Srivastava <adityasrivastav9721057380@gmail.com>
2025-05-06 10:52:53 +0530

    logo modified & inrease size

--- components/header.jsx
+++ components/header.jsx
@@ -10,14 +10,14 @@ const Header = async () => {
 
   return (
     <div className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b">
-      <nav className="container mx-auto px-4 py-4 flex items-center justify-between">
+      <nav className="container mx-auto px-4 h-20 flex items-center justify-between">
         <Link href="/">
           <Image
             src={"/logo.png"}
             alt="FinSight AI logo"
             height={60}
             width={200}
-            className="h-12 w-auto object-contain"
+            className="h-18 w-auto object-contain"
           />
         </Link>
 
